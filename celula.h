55dcc2c675bc975652799f2b4dfa6a31d15d75c1
Javi G <alu0101548197@ull.edu.es>
2024-02-04 22:19:20 +0000

    mejora

---
+++ celula.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <string.h>
+#include <utility>
+#include <set>
+#include <vector>
+
+class Celula {
+ public:
+
+ private:
+   //espacio celula
+   //Un conjunto de estados o alfabeto
+   //vecindad
+   //Una función de transición local
+}
---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void Help() {
+  std::cout << "-size <n>, n es el tamaño del retículo. Número de células." << std::endl;
+  std::cout << "-border <b [v]>, b=open, v=[0|1]. Frontera abierta, fría o caliente. b=periodic" << std::endl;
+  std::cout << "[-init <file>], (opcional) file es un nombre del fichero que contiene un array de estados con la configuración inicial del autómata celular." << std::endl;
+  std::cout << "Si no se especifica se utilizará la configuración inicial por defecto, esto es, un «1» en la célula central del retículo" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  
+
+}
